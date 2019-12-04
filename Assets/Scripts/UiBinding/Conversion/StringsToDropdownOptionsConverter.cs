4ae2dff712e358d659f1a6ea64a6b149a7ee8d77
Malte <malteweiss93@gmail.com>
2019-12-04 22:30:33 +0100

    Adds dropdown converter

---
+++ Assets/Scripts/UiBinding/Conversion/StringsToDropdownOptionsConverter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine.UI;
+
+namespace UiBinding.Conversion
+{
+    public class StringsToDropdownOptionsConverter : ValueConverter<IEnumerable<string>, List<Dropdown.OptionData>>
+    {
+        public override List<Dropdown.OptionData> Convert(IEnumerable<string> value)
+        {
+            return value.Select(s => new Dropdown.OptionData(s)).ToList();
+        }
+    }
+}
