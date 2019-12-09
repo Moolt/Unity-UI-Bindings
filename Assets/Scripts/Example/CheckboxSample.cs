96c8ffd394706f37b45f58fc55018277514451a0
Malte <malteweiss93@gmail.com>
2019-12-09 22:12:21 +0100

    Adds more samples

---
+++ Assets/Scripts/Example/CheckboxSample.cs
@@ -0,0 +1,19 @@
+﻿using UiBinding.Core;
+
+public class CheckboxSample : BindableMonoBehaviour
+{
+    private bool _checked;
+    private string _text = string.Empty;
+
+    public bool Checked
+    {
+        get => _checked;
+        set => SetField(ref _checked, value);
+    }
+
+    public string Text
+    {
+        get => _text;
+        set => SetField(ref _text, value);
+    }
+}
---
+++ Assets/Scripts/Example/ImageSample.cs
@@ -0,0 +1,23 @@
+﻿using UiBinding.Core;
+using UnityEngine;
+
+public class ImageSample : BindableMonoBehaviour
+{
+    [SerializeField] private Sprite _offSprite;
+    [SerializeField] private Sprite _onSprite;
+
+    private bool _checked;
+    private string _text = string.Empty;
+
+    public bool Checked
+    {
+        get => _checked;
+        set
+        {
+            SetField(ref _checked, value);
+            RaisePropertyChanged(() => Sprite);
+        }
+    }
+
+    public Sprite Sprite => _checked ? _onSprite : _offSprite;
+}
