92c0f0d0e7db0770d156533b581de7eb67bca63f
Malte <malteweiss93@gmail.com>
2019-12-18 20:55:11 +0100

    Adds float -> quaternion converter

---
+++ Assets/Scripts/UiBinding/Conversion/FloatToRotationConverter.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace UiBinding.Conversion
+{
+    public class FloatToRotationConverter : ValueConverter<float, Quaternion>
+    {
+        public override Quaternion Convert(float value)
+        {
+            return Quaternion.Euler(RotationAxis * value * 360);
+        }
+
+        public Vector3 RotationAxis { get; set; } = Vector3.back;
+    }
+}
