38934c813424d2f8816edec687af84b06ecabe93
Malte Weiß <malteweiss@yahoo.de>
2019-12-28 17:52:14 +0100

    Now supporting inheritance

--- Assets/Scripts/UiBinding/MemberFilters.cs
+++ Assets/Scripts/UiBinding/MemberFilters.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using UnityEngine;
 using UnityEngine.Events;
@@ -9,7 +9,7 @@ namespace UiBinding.Core
 {
     public static class MemberFilters
     {
-        private static readonly IList<PropertyInfo> MonoBehaviourTypes = typeof(GameObject).GetProperties(MemberBindingFlags.Source);
+        private static readonly IList<PropertyInfo> MonoBehaviourTypes = typeof(MonoBehaviour).GetProperties(MemberBindingFlags.Source);
 
         private static class MemberBindingFlags
         {
@@ -18,7 +18,7 @@ namespace UiBinding.Core
             public const BindingFlags Target = BindingFlags.Public | BindingFlags.Instance;
         }
 
-        public static PropertyFilter SourceProperties { get; } = new PropertyFilter(MemberBindingFlags.Source, m => !MonoBehaviourTypes.Contains(m));
+        public static PropertyFilter SourceProperties { get; } = new PropertyFilter(MemberBindingFlags.Source, m => !IsMonoBehaviourProperty(m));
 
         public static PropertyFilter TargetProperties { get; } = new PropertyFilter(MemberBindingFlags.Target);
 
@@ -26,6 +26,11 @@ namespace UiBinding.Core
 
         public static PropertyFilter TargetEvents { get; } = new PropertyFilter(MemberBindingFlags.Target, m => typeof(UnityEventBase).IsAssignableFrom(m.PropertyType));
 
-        public static PropertyFilter Lists { get; } = new PropertyFilter(MemberBindingFlags.Source, m => typeof(IEnumerable).IsAssignableFrom(m.PropertyType) && m.PropertyType != typeof(string));
+        public static PropertyFilter Lists { get; } = new PropertyFilter(MemberBindingFlags.Source, m => typeof(IEnumerable).IsAssignableFrom(m.PropertyType) && m.PropertyType != typeof(string) && !IsMonoBehaviourProperty(m));
+
+        private static bool IsMonoBehaviourProperty(PropertyInfo property)
+        {
+            return MonoBehaviourTypes.Any(p => p.Name == property.Name);
+        }
     }
 }
